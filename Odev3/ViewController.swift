06061933e6f4cdf6eee0eab4d61c17594940ecee
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2025-04-24 13:46:02 +0300

    Initial Commit

---
+++ Odev3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev3
+//
+//  Created by Şakir Yılmaz ÖĞÜT on 24.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
